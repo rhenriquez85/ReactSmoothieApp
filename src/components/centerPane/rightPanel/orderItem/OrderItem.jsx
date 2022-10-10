cd9ed784267c13d242c8f65e2ac2434e8eb31422
Rene Henriquez <ajrosado@Renes-MacBook-Pro.local>
2022-10-10 09:16:43 -0400

    [order item] edit styling

--- src/components/centerPane/rightPanel/orderItem/OrderItem.jsx
+++ src/components/centerPane/rightPanel/orderItem/OrderItem.jsx
@@ -25,6 +25,7 @@ const Container = styled.div`
     .name a:hover {
         color: darkorange;
         cursor: pointer;
+        font-weight: 600;
     }
 
     @media (pointer:coarse), (pointer:none), (max-width: 800px) {
